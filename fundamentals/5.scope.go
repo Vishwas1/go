29caef4b769d3231d316d05aa04b509fe3c75c63
Vishwas Anand <vishwasanand.cse@gmail.com>
2022-01-18 15:18:56 +0530

    fundamentals: scope

---
+++ fundamentals/5.scope.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Package level scope
+var result int
+
+//// Syntax 1
+func add(x int, y int) int {
+
+	// Function level scope
+	var res int = x + y
+	result = res
+
+	return res
+}
+
+func main() {
+
+	num1 := 5
+	num2 := 4
+
+	// prints 9
+	fmt.Println(add(num1, num2))
+
+	// prints 9
+	fmt.Println(result)
+}
