0a2094baac8670beaa5f1ccd321320df981b9fa3
Vishwas Anand <vishwasanand.cse@gmail.com>
2022-01-18 11:52:14 +0530

    fundamenta:  variable

---
+++ fundamentals/2.variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//// syntax 1
+	var i, j int
+
+	// this will print 0
+	fmt.Println(i + j)
+
+	//// syntax 2
+	var a, b int = 5, 10
+
+	// this will print 15
+	fmt.Println(a + b)
+
+	//// syntax 3:  same as var x = 10
+	x := 10
+
+	// this will print 25
+	fmt.Println(a + b + i + j + x)
+
+	/// Const
+	const ENV = "YOU CAN NOT CHANGE THIS"
+	fmt.Println(ENV)
+
+}
