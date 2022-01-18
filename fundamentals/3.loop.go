3dfd9a4b21f2991a62f7b41e0532d47fed98d64e
Vishwas Anand <vishwasanand.cse@gmail.com>
2022-01-18 11:52:51 +0530

    fundamentals: loops

---
+++ fundamentals/3.loop.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//// Syntax 1: Looping infinite times
+	// for {
+	// 	fmt.Println("Printing multiple times")
+	// }
+
+	//// Syntax 2: Looping finite times
+	fmt.Println("Using loop in lengthy way...")
+	i := 1
+	for i <= 5 {
+		fmt.Println("Printing i = ", i)
+		i++
+	}
+
+	//// Syntax 3: Looping finite times, more compact way
+	fmt.Println("Using loop in compact way...")
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Printing i = ", i)
+	}
+
+}
