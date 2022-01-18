e5f09f218129b4af621d3e851d0f7a0d8a8b8c34
Vishwas Anand <vishwasanand.cse@gmail.com>
2022-01-18 15:19:18 +0530

    fundamentals: Maths librarry

---
+++ fundamentals/7.math.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x float64 = 26
+	result := math.Sqrt(x)
+	fmt.Println("Square root of", x, "=", result)
+
+	// printing till 2 decimal places
+	fmt.Printf("Square root is %.2f", result)
+
+	fmt.Println()
+
+	// floor
+	fmt.Println("Square root of", x, "in flor=", math.Floor(math.Sqrt(x)))
+
+	fmt.Println("Square root of", x, "=", math.Ceil(math.Sqrt(x)))
+
+	fmt.Println("Square root of", x, "=", math.Round(math.Sqrt(x)))
+}
