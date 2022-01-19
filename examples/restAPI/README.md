fb91b1166fa5c423d41df7c931de0cab4427806f
Vishwas Anand <vishwasanand.cse@gmail.com>
2022-01-19 15:35:06 +0530

    example:  a simple rest API server

---
+++ examples/restAPI/README.md
@@ -0,0 +1,11 @@
+## A simple API server in Go
+
+
+## Tutorial:  https://go.dev/doc/tutorial/web-service-gin
+
+## APIs
+
+- `GET`  http://localhost:8080
+- `GET`  http://localhost:8080/:id
+- `POST`  http://localhost:8080
+
---
+++ examples/restAPI/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	// TODO: Route: Logic to prepare a response
+	router.GET("/", getAlbums)
+	router.GET("/:id", getAlbumById)
+	router.POST("/", addAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// TODO: Controller / handler: Code to map the request path to your logic
+func getAlbums(c *gin.Context) {
+	// serialise the josn and add it to response
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, album := range albums {
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+}
+
+func addAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
